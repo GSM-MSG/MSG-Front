803deabd3d972f5e4f4c2881d23a87a5f1ca57ce
변찬우 <chan00106@naver.com>
2022-03-02 23:47:59 +0900

    Add Main Page layout 배치

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,12 +1,63 @@
+/* eslint-disable @next/next/link-passhref */
+import Link from "next/link";
 import Header from "../Header";
 
 import * as S from "./styles";
 
 export default function MainPage() {
   return (
-    <S.Wrapper>
+    <>
       <Header />
-      MainPage
-    </S.Wrapper>
+      <S.Wrapper>
+        <S.Group>
+          <S.GroupInfo>
+            <S.GroupName>전공 동아리</S.GroupName>
+            <S.GroupAllNumber>총 30개</S.GroupAllNumber>
+          </S.GroupInfo>
+          <Link href="/major">
+            <S.AllViewLink>모두 보기</S.AllViewLink>
+          </Link>
+        </S.Group>
+        <S.Cards>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+        </S.Cards>
+        <S.Group>
+          <S.GroupInfo>
+            <S.GroupName>자율 동아리</S.GroupName>
+            <S.GroupAllNumber>총 30개</S.GroupAllNumber>
+          </S.GroupInfo>
+          <Link href="/major">
+            <S.AllViewLink>모두 보기</S.AllViewLink>
+          </Link>
+        </S.Group>
+        <S.Cards>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+        </S.Cards>
+        <S.Group>
+          <S.GroupInfo>
+            <S.GroupName>사설 동아리</S.GroupName>
+            <S.GroupAllNumber>총 30개</S.GroupAllNumber>
+          </S.GroupInfo>
+          <Link href="/major">
+            <S.AllViewLink>모두 보기</S.AllViewLink>
+          </Link>
+        </S.Group>
+        <S.Cards>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+          <S.Card></S.Card>
+        </S.Cards>
+      </S.Wrapper>
+    </>
   );
 }
