7817e63bf75133d56c7acc01393c47c7e256c8cd
변찬우 <chan00106@naver.com>
2022-04-03 23:23:28 +0900

    Add freedom, major user

--- src/pages/freedom/[clubName]/index.tsx
+++ src/pages/freedom/[clubName]/index.tsx
@@ -1,11 +1,10 @@
 import { useRouter } from "next/router";
-import Header from "../../components/Header";
-import InfoPage from "../../components/InfoPage";
+import Header from "../../../components/Header";
+import InfoPage from "../../../components/InfoPage";
 
 export default function ClubInfo() {
   const router = useRouter();
   const { clubName } = router.query;
-
   return (
     <>
       <Header />
--- src/pages/major/[clubName]/index.tsx
+++ src/pages/major/[clubName]/index.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from "next/router";
-import Header from "../../components/Header";
-import InfoPage from "../../components/InfoPage";
+import Header from "../../../components/Header";
+import InfoPage from "../../../components/InfoPage";
 
 export default function ClubInfo() {
   const router = useRouter();
   const { clubName } = router.query;
+
   return (
     <>
       <Header />
