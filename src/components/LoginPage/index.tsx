2e1e29f7d205d9aff5e0c995e97fda7790c05d70
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-05-10 23:18:46 +0900

    로그인 페이지 여러가지 제거

--- src/components/LoginPage/index.tsx
+++ src/components/LoginPage/index.tsx
@@ -1,12 +1,7 @@
-/* eslint-disable jsx-a11y/alt-text */
-import { useRouter } from "next/router";
-
 import * as S from "./styles";
 import * as SVG from "../../SVG";
 
 export default function LoginPage() {
-  const router = useRouter();
-
   return (
     <S.Wrapper>
       <S.Contents>
@@ -18,14 +13,6 @@ export default function LoginPage() {
             관리의 모든것
           </S.Title>
         </S.Logo>
-        <S.Buttons>
-          <S.Signin onClick={() => router.push("login/signin")}>
-            로그인
-          </S.Signin>
-          <S.Signup onClick={() => router.push("login/signup")}>
-            회원가입
-          </S.Signup>
-        </S.Buttons>
       </S.Contents>
     </S.Wrapper>
   );
